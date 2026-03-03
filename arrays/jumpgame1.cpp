0a883d278a1b8489d7df92f5bb3023475ef1546d
sourab <sourabhjadav7@gmail.com>
2026-03-03 23:15:04 +0530

    dp solution for min jumps [ dp ]

--- arrays/jumpgame1.cpp
+++ arrays/jumpgame1.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <climits>
+#include <cstring>
 using namespace std;
 
 int minJumps(vector<int> &nums, int l)
@@ -10,9 +12,27 @@ int minJumps(vector<int> &nums, int l)
         jumps = min(jumps, 1 + minJumps(nums, i));
     return jumps;
 }
+
+// using dp solution
+int dp(vector<int> &nums) {
+    int n = nums.size();
+    int dp[n];
+    memset(dp, 0, sizeof(dp));
+    // dp[n-1] is 0
+    int jumps=INT_MAX;
+    for (int i=n-2; i>=0; i--) {
+        for (int j = i + 1; j <= i + nums[i] && j<n; j++) {
+            dp[i]=min(jumps,1+dp[j]);
+        }
+    }
+    return dp[0];
+}
 int main()
 {
     // vector<int> nums{2, 3, 1, 1, 2, 4, 2, 0, 1, 1};
-    vector<int> nums{2, 3, 1, 1, 4};
-    cout << minJumps(nums, 0);
+    //    vector<int> nums{2, 3, 1, 1, 4}; 
+    vector<int> nums = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+
+    //    cout << minJumps(nums, 0);
+    cout<<    dp(nums)    ;
 }
