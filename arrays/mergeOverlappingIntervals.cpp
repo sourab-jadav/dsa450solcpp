5dac714e4bb48dd320030522f11ccddd45d76b84
sourab <sourabhjadav7@gmail.com>
2026-03-06 22:41:44 +0530

    merge overlapping intervals method2 [ arrays ] [ sorting ] [ while ]

--- arrays/mergeOverlappingIntervals.cpp
+++ arrays/mergeOverlappingIntervals.cpp
@@ -23,20 +23,39 @@ int main() {
 
 
   vector<pair<int, int>> result;
-  result.push_back(intervals[0]);
 
-  for(int i=1;i<n;i++){
+  // method 1 
+//   for(int i=1;i<n;i++){
 
-    pair<int, int>& last = result.back();
-    pair<int, int>& curr = intervals[i];
+//     pair<int, int>& last = result.back();
+//     pair<int, int>& curr = intervals[i];
 
-    if (curr.first <= last.second) {
-      last.second = max(curr.second, last.second);
-    } else {
-        result.push_back(curr);
-    }
-}  
-  
+//     if (curr.first <= last.second) {
+//       last.second = max(curr.second, last.second);
+//     } else {
+//         result.push_back(curr);
+//     }
+// }
 
+  int i=0;
+
+  while (i<n) {
+      int j = i + 1;
+      while(j<n){
+          if (intervals[j].first < intervals[i].second) {
+              intervals[i].second = max(intervals[i].second, intervals[j].second);
+              j++;
+          }else {
+              break;
+          }
+      }
+      result.push_back(intervals[i]);
+    i=j;
+  }
+
+  cout<<"the values are as follows"<<endl;
+  for (int i = 0; i < result.size(); i++) {
+      cout<<result[i].first<<" "<<result[i].second<<endl;
+  }  
 
 }
