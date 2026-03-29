987347fff0c4361903e065459f3d959213e82900
sourab <sourabhjadav7@gmail.com>
2026-03-29 13:46:06 +0530

    3 way parititioning using 1 loop

--- arrays/three_way_partitioning.cpp
+++ arrays/three_way_partitioning.cpp
@@ -11,6 +11,33 @@ using namespace std;
  * 3. all element greater than b appear at the end
  */
 /**
+ * using one loop
+ */
+void solution(vector<int> vec, int a, int b) {
+  int n = vec.size();
+  int start =0;
+  int end=n-1;
+  for(int i=0;i<n;){
+      if (vec[i]<a) {
+          if (i==start) {
+              i++;
+              start++;
+          }
+          else {
+              swap(vec[start++],vec[i++]);
+          }
+      }
+      // if the current element is greater than range
+      // put it on next available greater position
+      else if (vec[i]>b) {
+          swap( vec[i],vec[end--]);
+      }else {
+          i++;
+      }
+    }
+}
+
+/**
  * one solution is
  * make two unnested loops
  * first loop swaps with all elements <a
