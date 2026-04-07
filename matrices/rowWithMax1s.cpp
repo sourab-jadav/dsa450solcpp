18f763dff4bf8665d2bee6c16b49c4d90b170b87
sourab <sourabhjadav7@gmail.com>
2026-04-07 14:09:59 +0530

    row with max 1's [ ATP ] [ matrices ] [ binary search ]

--- matrices/rowWithMax1s.cpp
+++ matrices/rowWithMax1s.cpp
@@ -4,23 +4,38 @@ using namespace std;
 
 /**
  * row with max 1's
- * [[1,1,1,1,1,1,1,1,1],[0,0,0,0,0,1,1,1,1],[0,0,1,1,1,1,1,1,1],[0,0,0,0,0,0,1,1,1],[0,0,0,1,1,1,1,1,1],[0,0,0,0,0,0,0,0,1],[0,1,1,1,1,1,1,1,1],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,1,1,1,1,1,1]...
+ * a matrix contains only 0's and 1's sorted row wise
+ * you need to find indice of first row that contains
+ * max number of 1's
  */
  int rowWithMax1s(vector<vector<int>> &arr) {
      int m=arr.size();
      int n = arr[0].size();
-     /**
-      * one idea would be to perform binary search of value
-      * =1 columnwise
-      */
-     // this solution resulted in time limit exceeded
-     int j=0;
      int result=-1;
-     while (j<n) {
+     int low=0;
+     int high=n-1;
+     /**
+      * the idea is to do binary search
+      * column wise
+      * for a specific column traverse from left to right
+      * looking for thee value 1
+      * if the value is found add that row index to result
+      * and move left side to solve further
+      */
+     while (low<=high) {
+         int mid=(low+high)/2;
+         bool flag=false;
        for (int i = 0; i < m; i++) {
-           if (arr[i][j]==1) {
-               return i;
+           if (arr[i][mid]==1) {
+               flag=true;
+               result=i;
+               break;
            }
+         }
+         if(flag){
+             high=mid-1;
+         }else{
+             low=mid+1;
          }
      }
      return result;
