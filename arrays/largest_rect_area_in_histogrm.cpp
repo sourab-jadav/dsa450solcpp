eb79e7213bcebf3b993b631ad2e78783edf73637
sourab <sourabhjadav7@gmail.com>
2026-04-11 11:34:27 +0530

    Largest rectangle in histogram M-3 1 cornercasenot-covered [ stack ]
    the test failed for {1,1} input
    expected is 2
    got is 1

--- arrays/largest_rect_area_in_histogrm.cpp
+++ arrays/largest_rect_area_in_histogrm.cpp
@@ -4,6 +4,7 @@
 #include <utility>
 #include <vector>
 #include <queue>
+#include <stack>
 
 
 using namespace std;
@@ -94,12 +95,97 @@ int sol2usingfor_loop(vector<int> &heights) {
     return result;
 }
 
+/**
+ * solution using stacks put first comes last
+ * if we know the smaller element on it's left
+ * and smaller element on it's right we can get the count
+ * let's create two functions
+ * one calculate prev smaller
+ * another calculate next smaller
+ */
+void printarr(vector<int> arr) {
+    for (auto val : arr) {
+        cout << val << " ";
+    }
+    cout<<endl;
+}
+int sol3usingstacks(vector<int> &heights) {
+    int n=heights.size();
+    vector<int>ps(n,-1);
+    vector<int>ns(n,-1);
+
+    // [60, 20, 50, 40, 10, 50, 60]
+    // get nearest bar on the left that is smaller than current
+    // get nearest bar on the right that is smaller than current
+    // we maintain a stack that keeps track of smaller element from left
+
+    stack<int>stk;
+    // we keep on popping out the elements from stack
+    // until heights[stk.top()] <= heights[i]
+    //
+    // now that we have the element which is closest smaller on left
+    // if stk is not empty do ps[i]=stk.top;
+    //
+    for (int i = 0; i < n; i++) {
+        while (!stk.empty() && heights[stk.top()] > heights[i]) {
+            stk.pop();
+        }
+        if (!stk.empty()) {
+            ps[i]=stk.top();
+        }
+        stk.push(i);
+    }
+    while (!stk.empty()) {
+        stk.pop();
+    }
+
+    // next smaller element
+    // you are going from left to right
+    // you arrive at an element which is smaller than the element
+    // which you already visited
+    // instead of complicating it can we traverse from right to left
+    for (int i = n - 1; i >= 0; i--) {
+        while (!stk.empty() && heights[stk.top()]> heights[i]) {
+            stk.pop();
+        }
+        if (!stk.empty()) {
+            ns[i]=stk.top();
+        }
+        stk.push(i);
+    }
+    int result{};
+    printarr(ps);
+    printarr(ns);
+    for (int i = 0; i < n; i++) {
+        int lc{},rc{};
+        // exclude the current element when adding the lc and rc
+        if (ps[i]==-1) {
+            lc+=i-0;
+        } else {
+            lc+=i-1-ps[i];
+        }
+        if (ns[i]==-1) {
+            rc+=n-1-i;
+        }else{
+            rc+=ns[i]-i-1;
+        }
+        cout << "for i = "<<i << " "<<lc+rc<<endl;
+
+        result=max(result,(lc+rc+1)*heights[i]);
+    }
+    return result;
+}
 
 int main(int argc, char *argv[])
 {
     // vector<int>vec= {60, 20, 50, 40, 10, 50, 60};//test case passed
-    vector<int>heights={3,5,1,7,5,9};
-    cout << sol2usingfor_loop(heights) << "\n";
+    // vector<int>heights={3,5,1,7,5,9};
+    // vector<int>heights={2,1,5,6,2,3}; // passed
+    vector<int>heights={1,1}; //failed
+    // cout << sol2usingfor_loop(heights) << "\n";
+    cout<<sol3usingstacks(heights);
+
+
 
 
 
