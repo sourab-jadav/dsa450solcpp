e151e13a92ef515c4957a61cbfa87f7460d095fe
sourab <sourabhjadav7@gmail.com>
2026-04-11 17:33:55 +0530

    largest rect matrix of 1's [ dp ] [ hard ]
    easy to understand difficult to have intuition

--- matrices/largest_rectangle_with1s_matrix.cpp
+++ matrices/largest_rectangle_with1s_matrix.cpp
@@ -1,6 +1,47 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
+
+
+// dp solution
+/**
+ * bit difficult to think
+ * we need width and height to create an area
+ * as we traverse rowwise from left to right
+ * we will store width possible to that i,j from the left(j=0)
+ *
+ * for every i,j we compute the height
+ * we go upwards from that cell and see which cell has the min width
+ * area is (min_width*height)
+ */
+int dp(vector<vector<int>>mat){
+    int m=mat.size();
+    int n=mat[0].size();
+    vector<vector<int>>memo(m,vector<int>(n,0));
+    int ans=0;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+          if (mat[i][j]==0) {
+              continue;
+          }
+          /**
+           * this below line of using previous width value is important
+           */
+          memo[i][j]=(j==0)?1:memo[i][j-1]+1;
+          // we have set width of a rectangle
+          //
+          int width=memo[i][j];
+          for (int k = i; k >= 0; k--) {
+              width=min(width,memo[k][j]);
+              int area = width * (i-k+1);
+              ans=max(ans,area);
+          }
+      }
+    }
+    return ans;
+}
 
 /**
  * how do we select the base
