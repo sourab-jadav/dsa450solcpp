7097c727dddcdce0aa87145adc7e24aa640ca5b2
sourab <sourabhjadav7@gmail.com>
2026-04-27 00:36:19 +0530

    small optimization but still TLE

--- matrices/kthsmallestelementinrowwisematrix.cpp
+++ matrices/kthsmallestelementinrowwisematrix.cpp
@@ -18,31 +18,15 @@ using namespace std;
  */
 int main(int argc, char *argv[])
 {
-  // vector<vector<int>> mat {
-  //     {10,20,30,40},
-  //     {15,25,35,45},
-  //     {24,29,37,48},
-  //     {32,33,39,50}
-  // };
-
-  // vector<vector<int>>mat {{16, 28, 60, 64},
-  //                         {22, 41, 63, 91},
-  //                         {27, 50, 87, 93},
-  //                         {36, 78, 87, 94}};
 
   // vector<vector<int>>mat{{10, 20, 30, 40},
   //                        {15, 25, 35, 45},
   //                         {24, 29, 37, 48},
   //                         {32, 33, 39, 50}};
-    vector<vector<int>>mat
-        {
-            {
-                8,12
-            },
-            {
-                10,18
-            }
-        };
+vector<vector<int>>mat {{16, 28, 60, 64},
+                        {22, 41, 63, 91},
+                         {27, 50, 87, 93},
+                         {36, 78, 87, 94}};
 
   int n=mat.size();
   // another idea is
@@ -51,33 +35,30 @@ int main(int argc, char *argv[])
   indices.push_back(make_pair(0, 0));
   vector<vector<bool>>visited(n,vector<bool>(n,false));
   visited[0][0]=true;
-  int rq=4;
+  int rq=3;
   while (indices.size()!=rq) {
-      vector<pair<int, int>>temp;
-      for (auto indice : indices) {
-          int i=indice.first;
-          int j=indice.second;
-          if (i+1<n && visited[i+1][j]==false) {
-              temp.push_back(make_pair(i+1, j));
-          }
-          if (j+1<n && visited[i][j+1]==false) {
-              temp.push_back(make_pair(i, j+1));
-          }
-      }
+      vector<pair<int, int>> temp;
       pair<int, int>smallest
           {
               -1,-1
           };
-      for (auto p : temp) {
-          int i=p.first;
-          int j=p.second;
-          if (smallest.first==-1 || mat[i][j] < mat[smallest.first][smallest.second]) {
+      for (auto indice : indices) {
+          int i=indice.first;
+          int j=indice.second;
+          if (i+1<n && visited[i+1][j]==false && (smallest.first==-1|| mat[i+1][j] < mat[smallest.first][smallest.second])) {
               smallest=
                   {
-                      i,j
+                      i+1,j
+                  };
+          }
+          if (j+1<n && visited[i][j+1]==false && (smallest.first==-1|| mat[i][j+1]< mat[smallest.first][smallest.second])) {
+              smallest=
+                  {
+                      i,j+1
                   };
           }
       }
+
       visited[smallest.first][smallest.second]=true;
       indices.push_back(smallest);
       cout << "the smallest value is "<<smallest.first <<" "<<smallest.second << "\n";
