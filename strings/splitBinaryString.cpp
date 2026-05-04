d41ba2fb485627faaf5261f04e881f5ca8bcf28f
sourab <sourabhjadav7@gmail.com>
2026-05-04 18:25:47 +0530

    ATP ;) for splitBinaryString

--- strings/splitBinaryString.cpp
+++ strings/splitBinaryString.cpp
@@ -4,56 +4,42 @@
 using namespace std;
 
 int method1(string s) {
-    int n = s.size();
-    int i=0;
+    int n=s.size();
+    if (n%2!=0) {
+        return -1;
+    }
+    int ocount={};
+    int zcount={};
     int result{};
-    while (i<n) {
-        char ch=s[i];
-        int count=1;
-        if (ch=='0') {
-            int j=++i;
-            while (j<n && s[j]=='0') {
-                count++;
-                j++;
-            }
-            while (count>0 && j<n && s[j]=='1') {
-                count--;
-                j++;
-            }
-            if (count>0) {
-                return -1;
-            }else {
-                result++;
-            }
-            i=j;
-        }else if(ch=='1'){
-            int j=++i;
-            while (j<n && s[j]=='1') {
-                count++;
-                j++;
-            }
-            while (count>0 && j<n && s[j]=='0') {
-                count--;
-                j++;
-            }
-            if (count>0) {
-                return -1;
-            } else {
-                result++;
-            }
-            i=j;
+    int cnt{};
+    for (int i = 0; i < n; i++) {
+        if (s[i]=='1') {
+            ocount++;
+        }else {
+            zcount++;
+        }
+        if (ocount==zcount) {
+            result++;
+            cnt+=ocount*2;
+            ocount=0;
+            zcount=0;
         }
     }
-    return result;
-}
+    if (cnt==n) {
+        return result;
+    }else{
+        return -1;
+    }
+  }
 
 int main(int argc, char *argv[])
 {
     // string str = "00000000000"; //passed
     // string str="0111100010";    // failed
     // string str="001110010";
-    string str="101000101101010011"; // failed
-                                     // expected 6 got -1;
+    string str = "101000101101010011";
+    cout << str.size() << "\n";
+
 cout<<method1(str)<<endl;
 
     return 0;
