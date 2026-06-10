a819b8c42ac573b52d722554bf28e7d04e8543c2
sourab <sourabhjadav7@gmail.com>
2026-06-10 18:55:48 +0530

    rearrange characters using map and priority queue [ stl ]

--- strings/rearrangechar.cpp
+++ strings/rearrangechar.cpp
@@ -4,71 +4,55 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <queue>
+
 
 
 
 
 using namespace std;
 
+
+string rearrangeChar(string &s) {
+    priority_queue<pair<int, int>>pq;
+    unordered_map<char, int>mp;
+    int n=s.size();
+    for (auto v : s) {
+        mp[v]++;
+    }
+    for (auto t : mp) {
+        pq.push({t.second,t.first});
+    }
+    string res="";
+    pair<int, int>prev={-1,'#'};
+    while (!pq.empty()) {
+        pair<int, int>p=pq.top();
+        pq.pop();
+        res.push_back(p.second);
+
+        if (prev.first>0) {
+            pq.push(prev);
+        }
+        p.first--;
+        prev=p;
+    }
+    if (res.size()!=n) {
+        return "";
+    }
+    return res;
+}
+
 int main(int argc, char *argv[])
 {
     // string s = "aaabc";
     // string s=  "aaaabc";
-    // string s="aaabb";
+    string s="aaabb";
 
-    string s = "acbcbcc";
+    // string s = "acbcbcc";
     int n=s.length();
     unordered_map<char, int>mp;
-    for (auto va : s) {
-        mp[va]++;
-    }
-    vector<pair<char, int>>vec(mp.begin(),mp.end());
 
-    sort(vec.begin(), vec.end(), [](pair<char, int> &a, pair<char, int> &b) {
-        return a.second>b.second;
-    });
-
-    for (auto t : vec) {
-        cout << t.first <<" "<<t.second << "\n";
-    }
-
-    int m=vec.size();
-    string temp = "";
-    int i=0;
-    int j=1;
-    // we have to fill the temp with mp values
-    // alternatively
-    // a 3
-    // b 1
-    // c 1  => a b a c a
-    while (j<m) {
-        if (j<m && vec[j].second!=0) {
-            temp.push_back(vec[i].first);
-            vec[i].second-=1;
-            temp.push_back(vec[j].first);
-            vec[j].second-=1;
-        } else {
-            if (vec[i].second==0) {
-                i=j+1;
-                j=i+1;
-            }else{
-                j=j+1;
-            }
-        }
-    }
-    while (vec[i].second!=0 ) {
-        temp.push_back(vec[i].first);
-        vec[i].second-=1;
-    }
-
-    cout << temp << "\n";
-
-
-
-
-
-
-
+    rearrangeChar(s);
 
     return 0;
 }
