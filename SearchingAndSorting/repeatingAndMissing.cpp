2281fcbded6097aeaed5e58f25d068bd8815d733
sourab <sourabhjadav7@gmail.com>
2026-06-20 22:26:05 +0530

    repeating and missing ATP;) [ tricky ] [ arrays ]

--- SearchingAndSorting/repeatingAndMissing.cpp
+++ SearchingAndSorting/repeatingAndMissing.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -16,23 +17,24 @@ int main(int argc, char *argv[])
     int arr[]{4,3,6,2,1,1};
     int n=sizeof(arr)/sizeof(arr[0]);
 
-    // simple idea is to sort the array
-    // and do one traversal to find out the numbers
-
-    // we can find the sum of elements
-    // how do i find the one that is repeating
-    // you can find using the count array
-    vector<int>count(n,0);
-    int arr_total{};
-    int repeating_element=-1;
-    for(int i=0;i<n;i++){
-        if (count[arr[i]-1]==0) {
-            count[arr[i]-1]++;
-            arr_total+=arr[i];
+    // first finding the repeated element
+    int repeating{};
+    for (int i = 0; i < n; i++) {
+        int val=abs(arr[i]);
+        if (arr[val-1]>0) {
+            arr[val-1]=-arr[val-1];
+        } else {
+            repeating=val;
         }
-      }
-    int total=(n*(n+1))/2;
-    int missing_element=total-arr_total;
-
+    }
+    int remaining{};
+    // now if arr[i] is +ve means i+1th element is not visited
+    // if an element is not visited then it must not be present in the array
+    // since we have already visited all the array elements
+    for (int i = 0; i < n; i++) {
+        if (arr[i]>0) {
+            remaining=i+1;
+        }
+    }
     return 0;
 }
